1a5c32de2ae400f230d4c056ebb935b1e6ee1d3f
shubhambhojane98 <shubhambhojane90@gmail.com>
2026-04-07 15:14:28 +0530

    image updated

--- components/Main.tsx
+++ components/Main.tsx
@@ -27,7 +27,7 @@ const Main = () => {
         </Link>
       </div>
       <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-        <Image alt="image" src={deved} fill objectFit="cover" />
+        <Image alt="image" src={deved} fill style={{ objectFit: "cover" }} />
       </div>
     </div>
   );
